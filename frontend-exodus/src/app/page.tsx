56b5721440f1524c9f474e5588d92f3cd46d37d6
GmedicJaimes <akiloty3@gmail.com>
2025-05-16 16:37:37 -0500

    pantalla login terminada

--- frontend-exodus/src/app/page.tsx
+++ frontend-exodus/src/app/page.tsx
@@ -45,36 +45,50 @@ export default function Login() {
     setError(""); // Limpiar el error al enfocar el campo de entrada
   };
   return (
-    <div className="min-h-screen bg-[#00b4d8] flex  items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-      <div className="w-1/2 max-w-sm h-[600px] bg-[url('/loogin.webp')] bg-cover rounded-l-lg"></div>
-      <div className="w-1/2 max-w-xl h-[600px]  space-y-8 border-solid border-2 rounded-r-lg p-6 shadow-xl bg-white">
+    <div className="min-h-screen  bg-[#d8f3dc] flex  items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+      <div className="w-1/2 max-w-sm h-[600px] bg-[url('/login.jpg')] bg-cover rounded-l-lg"></div>
+      <div className="w-1/2 max-w-xl h-[600px]  space-y-8 border-solid border-2 rounded-r-lg p-16 shadow-xl bg-white">
         <div>
-          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-            Iniciar sesión en la plataforma
+          <h2 className="mt-6 text-center text-3xl font-extrabold text-black">
+            Iniciar sesión
           </h2>
         </div>
 
-        <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
+        <div className="border border-emerald-700 p-4 text-center rounded-lg bg-[#d8f3dc] m-0">
+          <p className="text-left text-sm font-ligh text-green-700">
+            Para iniciar sesión, ingrese el usuario "admin" y la contraseña
+            "admin123--"
+          </p>
+        </div>
+
+        <form className="mt-6 space-y-6" onSubmit={handleSubmit}>
           <div className="rounded-md -space-y-px">
             <div className="mb-4">
-              <label htmlFor="email" className="sr-only">
-                Email
+              <label
+                htmlFor="usuario"
+                className="text-black mb-2 text-sm font-semibold"
+              >
+                Usuario
               </label>
+
               <input
-                id="email"
-                name="email"
+                id="usuario"
+                name="usuario"
                 type="text"
-                autoComplete="email"
+                autoComplete="usuario"
                 required
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
                 onFocus={handleInputFocus}
-                className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
-                placeholder="User"
+                className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-400 bg-gray-100 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-blue-500 focus:border-emerald-700 focus:z-10 sm:text-sm"
+                placeholder="Usuario"
               />
             </div>
             <div>
-              <label htmlFor="password" className="sr-only">
+              <label
+                htmlFor="password"
+                className="text-black mb-2 text-sm font-semibold"
+              >
                 Contraseña
               </label>
               <input
@@ -85,7 +99,7 @@ export default function Login() {
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
                 onFocus={handleInputFocus}
-                className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm"
+                className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-400 bg-gray-100 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-blue-500 focus:border-emerald-700 focus:z-10 sm:text-sm"
                 placeholder="************"
               />
             </div>
@@ -104,9 +118,11 @@ export default function Login() {
             <button
               type="submit"
               disabled={loading}
-              className={`group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white ${
-                loading ? "bg-blue-400" : "bg-blue-600 hover:bg-blue-700"
-              } focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500`}
+              className={`group relative mt-6  w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white text-bold cursor-pointer ${
+                loading
+                  ? "bg-emerald-700"
+                  : "bg-emerald-600 hover:bg-emerald-700"
+              } focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-emerald-700`}
             >
               {loading ? "Iniciando sesión..." : "Iniciar sesión"}
             </button>
@@ -115,7 +131,7 @@ export default function Login() {
           <div className="text-sm text-center">
             <Link
               href="/forgot-password"
-              className="font-medium text-blue-600 hover:text-blue-500"
+              className="font-medium text-emerald-700 hover:text-emerald-800"
             >
               ¿Olvidaste tu contraseña?
             </Link>
